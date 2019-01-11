7e398fffdc61b517fc8687b843862866c0d89abf
Aaron <303653981@qq.com>
2019-01-11 11:01:43 +0800

    update Sync.php

--- application/controllers/api/Sync.php
+++ application/controllers/api/Sync.php
@@ -61,16 +61,17 @@ class Sync extends BaseController {
                         echo json_encode(array('code'=>-1,'msg'=>'book_code:' + $post['book_code'] + ' not exist!'));
                         return;
                     }
-                    $cash_pool_code = $this->book[trim($post['book_code'])]['CashpoolCode'];
-                    $supplier       = $post['supplier'];
-                    $vendorcode     = $post['vendorcode'];
-                    $vendorstatus   = 0;
-                    $relevancyemail = $post['relevancyemail'];
+                    $cash_pool_code  = $this->book[trim($post['book_code'])]['CashpoolCode'];
+                    $supplier        = $post['supplier'];
+                    $vendorcode      = $post['vendorcode'];
+                    $payment_days    = $post['payment_days'];
+                    $vendorstatus    = 0;
+                    $relevancy_email = $post['relevancy_email'];
                     if (isset($post['id'])) {
                         $id = $post['id'];
-                        $sql_arr[] = "UPDATE Customer_Suppliers SET CreateUser='{$create_user}',CashpoolCode='{$cash_pool_code}',Supplier='{$supplier}',Vendorcode='{$vendorcode}',VendorStatus={$vendorstatus},RelevancyEmail='{$relevancyemail}' WHERE Id={$id};"; 
+                        $sql_arr[] = "UPDATE Customer_Suppliers SET CreateUser='{$create_user}',CashpoolCode='{$cash_pool_code}',Supplier='{$supplier}',Vendorcode='{$vendorcode}',VendorStatus={$vendorstatus},RelevancyEmail='{$relevancy_email}',payment_days={$payment_days} WHERE Id={$id};"; 
                     } else {
-                        $sql_arr[] = "INSERT INTO Customer_Suppliers(Id,CreateUser,CashpoolCode,Supplier,Vendorcode,VendorStatus,RelevancyEmail)VALUES(uuid_short(),'{$create_user}', '{$cash_pool_code}', '{$supplier}', '{$vendorcode}', $vendorstatus, '{$relevancyemail}');";
+                        $sql_arr[] = "INSERT INTO Customer_Suppliers(Id,CreateUser,CashpoolCode,Supplier,Vendorcode,VendorStatus,RelevancyEmail,payment_days)VALUES(uuid_short(),'{$create_user}', '{$cash_pool_code}', '{$supplier}', '{$vendorcode}', $vendorstatus, '{$relevancy_email}', {$payment_days});";
                     }
                 }
                 if (count($sql_arr) > 0) {
@@ -104,16 +105,15 @@ class Sync extends BaseController {
                 $invoice_no     = $post['invoice_no'];
                 $invoice_amount = $post['invoice_amount'];
                 $invoice_date   = $post['invoice_date']; 
-                $payment_days   = $post['payment_days'];
                 $estPaydate     = $post['estPaydate'];
                 //$invoce_status  = 1;//default 1
                 $client_status  = $post['status'];
                 //$isincluded     = 1;//default 1
                 if (isset($post['id'])) {
                     $id = $post['id'];
-                    $sql_arr[] = "UPDATE Customer_Payments SET CashpoolCode='{$cash_pool_code}',Vendorcode='{$vendorcode}',InvoiceNo='{$invoice_no}',InvoiceAmount={$invoice_amount},InvoiceDate='{$invoice_date}',payment_days='{$payment_days}',EstPaydate='{$estPaydate}',client_status={$client_status} WHERE Id={$id}";
+                    $sql_arr[] = "UPDATE Customer_Payments SET CashpoolCode='{$cash_pool_code}',Vendorcode='{$vendorcode}',InvoiceNo='{$invoice_no}',InvoiceAmount={$invoice_amount},InvoiceDate='{$invoice_date}',EstPaydate='{$estPaydate}',client_status={$client_status} WHERE Id={$id}";
                 } else {
-                    $sql_arr[] = "INSERT INTO Customer_Payments(Id,CreateUser,CashpoolCode,Vendorcode,InvoiceNo,InvoiceAmount,InvoiceDate,payment_days,EstPaydate,client_status)VALUES(uuid_short(),'{$createUser}','{$cash_pool_code}','{$vendorcode}','{$invoice_no}',$invoice_amount,'{$invoice_date}',{$payment_days},'{$estPaydate}',{$client_status});";    
+                    $sql_arr[] = "INSERT INTO Customer_Payments(Id,CreateUser,CashpoolCode,Vendorcode,InvoiceNo,InvoiceAmount,InvoiceDate,EstPaydate,client_status)VALUES(uuid_short(),'{$createUser}','{$cash_pool_code}','{$vendorcode}','{$invoice_no}',$invoice_amount,'{$invoice_date}','{$estPaydate}',{$client_status});";    
                 }
             } 
             if (count($sql_arr) > 0) {
